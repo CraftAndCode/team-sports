3740f45453b3506cd0a64f13e390d71456ecae40
CraftAndCode <89014755+CraftAndCode@users.noreply.github.com>
2022-06-23 19:16:51 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,8 +17,7 @@ input.onButtonPressed(Button.B, function () {
         Pause = false
     }
 })
-let goal1 = 0
-let goal = 0
+let PauseTime = 0
 let Pause = false
 let RightPlayerScore = 0
 let LeftPlayerScore = 0
@@ -27,16 +26,12 @@ let RightPlayer = "Player 2"
 LeftPlayerScore = 0
 RightPlayerScore = 0
 let ScoreToWin = 5
-let MatchTime = 3
+let MatchTime = 60
 basic.showArrow(ArrowNames.North)
 while (!(input.logoIsPressed())) {
     basic.pause(1)
 }
 basic.clearScreen()
-proportionalFont.showString("" + LeftPlayer + " vs " + RightPlayer, 150)
-proportionalFont.showSpace(5, 150)
-basic.pause(500)
-basic.showString("GO!")
 music.setVolume(255)
 music.playTone(466, music.beat(BeatFraction.Whole))
 music.rest(music.beat(BeatFraction.Whole))
@@ -45,62 +40,79 @@ music.rest(music.beat(BeatFraction.Whole))
 music.playTone(466, music.beat(BeatFraction.Whole))
 music.rest(music.beat(BeatFraction.Whole))
 music.playTone(932, music.beat(BeatFraction.Double))
+basic.showString("GO!")
 basic.pause(1000)
 Pause = false
+let TimeLeft = MatchTime * 1000
 let StartTime = control.millis()
 basic.forever(function () {
+    pins.setPull(DigitalPin.P1, PinPullMode.PullDown)
+    pins.setPull(DigitalPin.P2, PinPullMode.PullDown)
     if (!(Pause)) {
-        goal = pins.digitalReadPin(DigitalPin.P1)
-        goal1 = pins.digitalReadPin(DigitalPin.P2)
-        if (goal1 == 0 && !(goal == 0)) {
-            Pause = true
-            RightPlayerScore += 1
-            music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
-            basic.showNumber(RightPlayerScore)
-            basic.pause(500)
-            goal1 = 0
-            Pause = false
+        if (pins.digitalReadPin(DigitalPin.P1) == 0 && !(pins.digitalReadPin(DigitalPin.P2) == 0)) {
+            for (let index = 0; index <= 50; index++) {
+                basic.pause(10)
+                if (!(pins.digitalReadPin(DigitalPin.P1) == 0) || pins.digitalReadPin(DigitalPin.P2) == 0) {
+                    break;
+                }
+                if (index == 50) {
+                    PauseTime = control.millis()
+                    Pause = true
+                    RightPlayerScore += 1
+                    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+                    basic.showNumber(RightPlayerScore)
+                    while (pins.digitalReadPin(DigitalPin.P1) == 0) {
+                        basic.pause(1)
+                    }
+                    Pause = false
+                    StartTime += control.millis() - PauseTime
+                }
+            }
         }
-        if (goal == 0 && !(goal1 == 0)) {
-            Pause = true
-            LeftPlayerScore += 1
-            music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-            basic.showNumber(LeftPlayerScore)
-            basic.pause(500)
-            goal = 0
-            Pause = false
+        if (pins.digitalReadPin(DigitalPin.P2) == 0 && !(pins.digitalReadPin(DigitalPin.P1) == 0)) {
+            for (let index = 0; index <= 50; index++) {
+                basic.pause(10)
+                if (!(pins.digitalReadPin(DigitalPin.P2) == 0) || pins.digitalReadPin(DigitalPin.P1) == 0) {
+                    break;
+                }
+                if (index == 50) {
+                    PauseTime = control.millis()
+                    Pause = true
+                    LeftPlayerScore += 1
+                    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+                    basic.showNumber(LeftPlayerScore)
+                    while (pins.digitalReadPin(DigitalPin.P2) == 0) {
+                        basic.pause(1)
+                    }
+                    Pause = false
+                    StartTime += control.millis() - PauseTime
+                }
+            }
         }
+        TimeLeft = MatchTime * 1000 - (control.millis() - StartTime)
     }
 })
 basic.forever(function () {
-    if (control.millis() > StartTime + MatchTime * 60000 || (LeftPlayerScore > ScoreToWin || RightPlayerScore > ScoreToWin)) {
+    if (TimeLeft <= 0 || (LeftPlayerScore > ScoreToWin || RightPlayerScore > ScoreToWin)) {
         Pause = true
         music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-        basic.showLeds(`
-            # # # # #
-            # . # . #
-            # . # . #
-            # . . . #
-            # # # # #
-            `)
-        basic.pause(1000)
         if (LeftPlayerScore > RightPlayerScore) {
             while (true) {
-                proportionalFont.showString("" + LeftPlayer + " wins!", 150)
+                proportionalFont.showString("" + LeftPlayer + " wins! ", 150)
             }
         } else if (LeftPlayerScore < RightPlayerScore) {
             while (true) {
-                proportionalFont.showString("" + RightPlayer + " wins!", 150)
+                proportionalFont.showString("" + RightPlayer + " wins! ", 150)
             }
         } else {
             while (true) {
-                proportionalFont.showString("Draw!", 150)
+                proportionalFont.showString(" Draw!", 150)
             }
         }
     } else if (!(Pause)) {
         led.plotBarGraph(
-        StartTime + MatchTime * 60000 - control.millis(),
-        MatchTime * 60000
+        TimeLeft,
+        MatchTime * 1000
         )
     }
 })
